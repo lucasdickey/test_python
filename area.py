f03ba0f6872c357f56b350b5477a81ececf103ff
Lucas Dickey <lucas@lucasdickey.com>
2011-08-14 21:43:19 -0700

    Adding two different area functions, where one is imported into the other. The latter also uses the distance module/script.

---
+++ area.py
@@ -0,0 +1,2 @@
+def area(radius):
+    return 3.14159 * radius**2
---
+++ area2.py
@@ -0,0 +1,5 @@
+import area
+import distance
+
+def area2(xc, yc, xp, yp):
+    return area.area(distance.distance(xc, yc, xp, yp))
